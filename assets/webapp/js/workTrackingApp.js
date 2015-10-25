6de5876444639b5c41a98240f672b35db38a1b50
Florian Loch <florian.loch@gmail.com>
2015-10-25 23:07:02 +0100

    Start building the webapp

---
+++ assets/webapp/js/workTrackingApp.js
@@ -0,0 +1,21 @@
+(function () {
+  var app = angular.module("workTrackingApp", ["ngRoute"]);
+
+  app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.
+    when("/login", {
+      templateUrl: "views/login_view.html",
+      controller: "LoginController"
+    }).
+    when("/workweek",  {
+      templateUrl: "views/workweek_view.html",
+      controller: "WorkweekController"
+    }).
+    when("/loading",  {
+      templateUrl: "views/loading_view.html"
+    }).
+    otherwise({
+      redirectTo: "/loading"
+    });
+  }]);
+})();
---
+++ assets/webapp/js/workTrackingController.js
@@ -0,0 +1,7 @@
+(function () {
+  var mod = angular.module("workTrackingApp");
+
+  mod.controller("MainController", ["$scope", function ($scope) {
+    
+  }]);
+})();
